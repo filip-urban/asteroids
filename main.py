c3e6cbdae9c1ee2ddbb3055dc2bd7c284b57ec14
filip-urban <filipurban93@gmail.com>
2025-02-27 20:12:16 +0100

    add player

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ def main():
     color = (0, 0, 0)
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color)
         pygame.display.flip()
+        player.draw(screen)
         dt = clock.tick(60) / 1000
 
 
